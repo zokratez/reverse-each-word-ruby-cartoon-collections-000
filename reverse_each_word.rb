5243ed98ce5379b787ef364f12b102e5fe097867
Sam Oteo <tortillabar@me.com>
2020-03-05 05:19:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,11 @@
 def reverse_each_word(sentence)
-  .each
+  sentence.each
   
 end
+
+#def reverse_each_word(sentence)
+##returns same sentence with each word reverse
+#  sentence.split.collect do |words|
+#    words.reverse
+#  end.join(" ") # this will join that array into a string, joining each element with a space " " :) since every word is divided by a space.
+#end
